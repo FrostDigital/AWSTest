5f056cc6cb7b3bb705b612c4736278ff75eec76c
Sergii Nezdolii <sergii.nezdolii@gmail.com>
2016-02-10 15:25:05 +0100

    Adding basic UI for AIK and DIF topics

--- AWSTest/Constants.swift
+++ AWSTest/Constants.swift
@@ -11,4 +11,5 @@ let DefaultServiceRegionType = AWSRegionType.EUWest1
 let CognitoIdentityPoolId = "eu-west-1:ce293d64-dad2-493e-a09e-881c822484c4"
 let SNSPlatformApplicationArn = "arn:aws:sns:eu-west-1:618717458389:app/APNS_SANDBOX/AWSTest_Dev"
 let SNSTopicAIKArn = "arn:aws:sns:eu-west-1:618717458389:AIK"
+let SNSTopicDIFArn = "arn:aws:sns:eu-west-1:618717458389:DIF"
 //let MobileAnalyticsAppId = "YourMobileAnalyticsAppId"
--- AWSTest/ViewController.swift
+++ AWSTest/ViewController.swift
@@ -12,7 +12,14 @@ class ViewController: UIViewController {
     @IBOutlet weak var deviceToken: UILabel?
     @IBOutlet weak var endpointArn: UILabel?
     @IBOutlet weak var userAction: UILabel?
+    @IBOutlet weak var aikSwitch: UISwitch?
+    @IBOutlet weak var difSwitch: UISwitch?
     
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        updateTopicsUI()
+    }
+
     func displayDeviceInfo() {
         deviceToken?.text = NSUserDefaults.standardUserDefaults().stringForKey("deviceToken") ?? "N/A"
         endpointArn?.text = NSUserDefaults.standardUserDefaults().stringForKey("endpointArn") ?? "N/A"
@@ -24,5 +31,18 @@ class ViewController: UIViewController {
         } else {
             userAction?.text = "The user selected [" + (action! as String) + "]"
         }
+    }
+    
+    func updateTopicsUI() {
+        aikSwitch?.on = NSUserDefaults.standardUserDefaults().boolForKey("aikSubscribed") ?? false
+        difSwitch?.on = NSUserDefaults.standardUserDefaults().boolForKey("difSubscribed") ?? false
+    }
+    
+    @IBAction private func switchAIK() {
+        print("Switch AIK: ", aikSwitch?.on)
+    }
+
+    @IBAction private func switchDIF() {
+        print("Switch DIF: ", difSwitch?.on)
     }
 }
